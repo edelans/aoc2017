8d6ae3b7c12ef651235df10867f286ff8d241a32
edelans <edelans@gmail.com>
2017-12-05 22:55:13 +0100

    part 2 ok

--- day5.py
+++ day5.py
@@ -34,7 +34,7 @@ def count_steps(l):
         counter += 1
 
     # print('index is {}, counter is {}, list is {}'.format(index, counter, str(l)))
-    print('Inscruction list exited after {} steps.'.format(counter))
+    # print('Inscruction list exited after {} steps.'.format(counter))
     return counter
 
 
@@ -42,3 +42,29 @@ assert parse_input(teststr) == [0, 3, 0, 1, -3]
 assert count_steps(parse_input(teststr)) == 5
 
 # count_steps([int(x) for x in Input(5)])
+
+
+# part 2
+
+
+def count_steps2(l):
+    counter = 0
+    index = 0
+    while index >= 0 and index < len(l):
+        # print('index is {}, counter is {}, list is {}'.format(index, counter, str(l)))
+        old_index = index
+        offset = l[index]
+        index = index + l[index]
+        if offset >= 3:
+            l[old_index] -= 1
+        else:
+            l[old_index] += 1
+        counter += 1
+
+    # print('index is {}, counter is {}, list is {}'.format(index, counter, str(l)))
+    print('Inscruction list exited after {} steps.'.format(counter))
+    return counter
+
+assert count_steps2(parse_input(teststr)) == 10
+
+count_steps2([int(x) for x in Input(5)])
