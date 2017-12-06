ffe889024b4d381a8b2e5b205318cea68b1285ea
edelans <edelans@gmail.com>
2017-12-06 01:01:29 +0100

    part 2 ok

--- day3.py
+++ day3.py
@@ -3,6 +3,8 @@
 import re
 import math
 
+from itertools import count
+
 ##################### PART 1 #########################
 
 input1 = 325489
@@ -12,32 +14,20 @@ def spiral_mem(square):
     n = find_square_index(square)
     x = (n - 1) / 2
     y = 0
-    #print('n is {}'.format(n))
     leg_length = n - 1
-    #print('leg_length is {}'.format(leg_length))
     half_side = int(leg_length / 2)
-    #print('half_side is {}'.format(half_side))
     if square < n**2 - 3*leg_length:
-        #print('leg 1')
         center = (n**2 - 3*leg_length - half_side)
-        #print('center is {}'.format(center))
         y = abs(square - center)
     elif square < n**2 - 2*leg_length:
-        #print('leg 2')
         center = (n**2 - 2*leg_length - half_side)
-        #print('center is {}'.format(center))
         y = abs(square - center)
     elif square < n**2 - 2*leg_length:
-        #print('leg 3')
         center = (n**2 - 1*leg_length - half_side)
-        #print('center is {}'.format(center))
         y = abs(square - center)
     else:
-        #print('leg 4')
         center = (n**2 - 0*leg_length - half_side)
-        #print('center is {}'.format(center))
         y = abs(square - center)
-    #print('x is {} and y is {}'.format(x, y))
     return int(x + y)
 
 
@@ -56,7 +46,63 @@ assert spiral_mem(23) == 2
 assert spiral_mem(1024) == 31
 
 
-print(spiral_mem(input1))
+# print(spiral_mem(input1))
 
 
 ##################### PART 2 #########################
+
+
+# compute sum of neighbors values
+def sum_8_neighbors(points, pos):
+    (i, j) = pos
+    res = 0
+    for k in range(i - 1, i + 2):
+        for l in range(j - 1, j + 2):
+            res += points.get((k, l), 0)
+    return res
+
+
+
+def neighbors8(point):
+    "The eight neighbors (with diagonals)."
+    x, y = point
+    return ((x+1, y), (x-1, y), (x, y+1), (x, y-1),
+            (x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1))
+
+
+def sum_spiral():
+    """generator for the values of the spiral"""
+
+    # store values of the spiral in a dict.
+    # key is coordinates
+    # set the spiral center to (0,0), with value of 1
+    a, i, j = {(0,0): 1}, 0, 0
+
+    # for each "square" that forms the spiral (defined by its odd side length)
+    # not really a square though, as the left side exceeds by 1
+    # see example below from S (start) to F (finish)
+    #
+    #   * < < < *
+    #   v       ^
+    #   v       ^
+    #   v S > > *
+    #   F
+    #
+    for s in count(1, 2):
+        # for each directions possible on that path
+        # right, up, left, down
+        for (ds, di, dj) in [(0, 1, 0), (0, 0, 1), (1, -1, 0), (1, 0, -1)]:
+            # for every atomic movement on that direction
+            for _ in range(s + ds):
+                i += di
+                j += dj
+                a[i, j] = sum(a.get((k, l), 0) for (k, l) in neighbors8((i, j)))
+                yield a[i, j]
+
+
+def part2(n):
+    for x in sum_spiral():
+        if x > n:
+            return x
+
+print(part2(input1))
