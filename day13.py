2a61dc86ea7383d66861ef898a239ce3213b08a3
edelans <edelans@gmail.com>
2017-12-13 19:27:31 +0100

    part 2 ok + refacto

--- day13.py
+++ day13.py
@@ -18,31 +18,51 @@ teststr = """0: 3
 6: 4"""
 
 
-def is_up(clock, depth):
-    return int(clock % (2 * (depth - 1)) == 0)
+def parse_firewall(lines):
+    firewall = {}
+    for line in lines:
+        depth, scan_range = map(int, line.split(': '))
+        firewall[depth] = scan_range
+    return firewall
+
+
+def is_caught(index, scan_range):
+    return index % (2 * (scan_range - 1)) == 0
 
 
 def solve1(lines):
-    firewall_depth = {}
-    for line in lines:
-        depth, scan_range = map(int, line.split(': '))
-        firewall_depth[depth] = scan_range
-    firewall_length = max(firewall_depth.keys()) + 1
-
+    firewall = parse_firewall(lines)
     severity = 0
-    for i in range(0, firewall_length):
-        severity += is_up(i, firewall_depth.get(i, 0)) * i * firewall_depth.get(i, 0)
+    for key, value in firewall.items():
+        severity += key * value if is_caught(key, value) else 0
     return severity
+
 
 assert solve1(teststr.splitlines()) == 24
 
-print("The result for part 1 is : {}".format(solve1(Input(13).readlines())))
+# print("The result for part 1 is : {}".format(solve1(Input(13).readlines())))
 
 
 # part 2
 
-#
-#
-# assert solve2(teststr.splitlines()) == 2
-#
-# print("The result for part 2 is : {}".format(solve2(Input(12).readlines())))
+
+def does_trigger(firewall, delay):
+    for key, value in firewall.items():
+        if (key + delay) % (2 * (value - 1)) == 0:
+            return True
+    return False
+
+
+def solve2(lines):
+    firewall = parse_firewall(lines)
+    delay = 0
+    while does_trigger(firewall, delay):
+        delay += 1
+    return delay
+
+
+# firewall = parse_firewall(Input(13).readlines())
+assert solve2(teststr.splitlines()) == 10
+
+
+# print("The result for part 2 is : {}".format(solve2(Input(13).readlines())))
