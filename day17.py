e9573106ec9ee43b7fe717ca9bba5440b7d23031
edelans <edelans@gmail.com>
2017-12-17 15:25:35 +0100

    part 2 ok

--- day17.py
+++ day17.py
@@ -24,23 +24,43 @@ assert spinlock([0, 2, 1], 1, 3, 3) == ([0, 2, 3, 1], 2)
 
 
 def solve1(offset):
-    l = [0]
+    insertions = [0]
     cur = 0
     for i in range(1, 2017 + 1):
-        l, cur = spinlock(l, cur, offset, i)
-    return l[(cur + 1) % len(l)]
+        insertions, cur = spinlock(insertions, cur, offset, i)
+    return insertions[(cur + 1) % len(insertions)]
 
 
-# assert(solve1(teststr.splitlines()) == XXXXX)
-
-
-res = solve1(382)
-print(res)
-
-
-#
-# def solve2(operations):
-#     l[l.index(0) + 1]
-#
-# res = solve2(parse_words(Input(DAY).readline()))
+# res = solve1(382)
 # print(res)
+
+
+### PART 2
+
+
+# def solve2(offset, total_insertions):
+#     insertions = [0]
+#     cur = 0
+#
+#     for i in range(1, total_insertions + 1):
+#         if (i % 500000) == 0:
+#             print(" i = {}, progress: {}% ".format(i, 100 * i / total_insertions))
+#         val_after_0 = i
+#         insertions, cur = spinlock(insertions, cur, offset, i)
+#     return val_after_0
+#
+#
+# res = solve2(382, 50000000)
+# print(res)
+
+
+# it's not necessary to keep the list in memory !!
+def next0(step):
+    pos = 0
+    final = 0
+    for i in range(1, 50000000 + 1):
+        pos = (pos + step) % i + 1
+        if pos == 1:
+            final = i
+    return(final)
+print('Value after 0 in completed buffer is', next0(382))
