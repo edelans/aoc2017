315a513774d87af7f3dcf237dab5f72156e510f2
edelans <edelans@gmail.com>
2017-12-17 01:34:19 +0100

    part 2 ok

--- day16.py
+++ day16.py
@@ -25,9 +25,8 @@ assert(spin([x for x in "abcde"], 3) == [x for x in "cdeab"])
 
 
 def exchange(progs, A, B):
-    tmp = progs[A]
-    progs[A] = progs[B]
-    progs[B] = tmp
+    # Python evaluates the right hand side of an assignment first so you can swap variables like this (no need for temp var)
+    progs[A], progs[B] = progs[B], progs[A]
     return progs
 
 
@@ -41,8 +40,8 @@ def partner(progs, A, B):
 assert(partner([x for x in "eabdc"], 'e', 'b') == [x for x in "baedc"])
 
 
-def solve1(operations):
-    progs = [x for x in "abcdefghijklmnop"]
+def solve1(alpha, operations):
+    progs = [x for x in alpha]
 
     for op in operations:
         # print(op)
@@ -54,24 +53,29 @@ def solve1(operations):
             progs = partner(progs, op[1:].split('/')[0], op[1:].split('/')[1])
     return ''.join(progs)
 
-res = solve1(parse_words(Input(16).readline()))
-# testres = solve1(teststr)
-print(res)
-# assert solve1(teststr) == 8108
 
-# print(solve1('ljoxqyyw'))
-
-
-def solve2(input):
-    pass
-
-
-# res, colored_dict = solve2('ljoxqyyw')
-
-# for i in range(0, 128):
-#     line = []
-#     for j in range(0, 128):
-#         line.append(colored_dict.get((i,j), 0))
-#     print(line)
-
+# res = solve1("abcdefghijklmnop", parse_words(Input(16).readline()))
 # print(res)
+
+
+
+def solve2(operations):
+    progs = [x for x in "abcdefghijklmnop"]
+    alpha = 'abcdefghijklmnop'
+    alpha = solve1(alpha, operations)
+
+    # let's find a cycle
+    i = 1
+    while alpha != 'abcdefghijklmnop':
+        alpha = solve1(alpha, operations)
+        i += 1
+
+    j = 1000000000 % i
+
+    for k in range(j):
+        alpha = solve1(alpha, operations)
+
+    return alpha
+
+res = solve2(parse_words(Input(16).readline()))
+print(res)
